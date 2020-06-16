8dbb989b08e2d1935d7e563aeb62b91a936971b1
Ayrton <ayrtong.guerra@gmail.com>
2020-06-16 11:20:35 -0400

    Projeto terminado com sucesso

---
+++ src/server.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const server = express();
+const nunjucks = require("nunjucks");
+const db = require("./database/db");
+
+// configurando nunjucks
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// Configurando a utilizaçao da pasta public
+
+server.use(express.static("public"));
+
+// habilitar o uso do req.body na aplicação
+server.use(express.urlencoded({extended: true}))
+
+// configurando rotas
+
+server.get("/", (req,res) => {
+    return res.render("index.html");
+});
+
+server.get("/create-point", (req,res) => {
+
+    // console.log(req.query);
+
+    return res.render("create-point.html");
+});
+
+server.post("/savepoint", (req,res) => {
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `;
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if(err) { 
+            console.log(err);
+            return res.render("create-point.html", { saved: false });
+        }
+        console.log('Cadastrado com sucesso!');
+        console.log(this);
+
+        return res.render("create-point.html", { saved: true });
+    }
+
+    db.run(query, values, afterInsertData);
+})
+
+server.get("/search-results", (req,res) => {
+
+    const search = req.query.search;
+        if(search == "") {
+        return res.render("search-results.html", {total: 0})
+    }
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`,  function(err, rows) {
+                if(err) return console.log(err);
+                const total = rows.length;
+                return res.render("search-results.html", { places:rows, total});
+            })
+    
+});
+
+const port = 3000;
+server.listen(port, () => console.log(`Servidor funcionando na porta:${port}`))
